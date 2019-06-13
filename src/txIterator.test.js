94958bf0afdb826eb82941b674d5fdb683fc4783
vanschroeder <van@webfreshener.com>
2019-06-13 12:23:41 -0700

    reactived iterator test

--- src/txIterator.test.js
+++ src/txIterator.test.js
@@ -1,10 +1,10 @@
 import {TxIterator} from "./txIterator";
 
-describe.skip("TxIterator Tests", () => {
+describe("TxIterator Tests", () => {
     it("should loop", () => {
         const _ = (
-            new TxIterator((_) => _*2)
-        ).loop([1,3,5]);
+            new TxIterator((_) => _ * 2)
+        ).loop([1, 3, 5]);
         expect(`${_}`).toEqual("2,6,10");
     });
 });
